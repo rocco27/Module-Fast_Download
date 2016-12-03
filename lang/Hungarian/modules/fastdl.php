ec9d546edb12938d027a01a53f6bf62911c1a56c
own3mall <own3mall@gmail.com>
2016-12-03 08:43:36 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Hungarian/modules/fastdl.php
+++ lang/Hungarian/modules/fastdl.php
@@ -22,7 +22,7 @@
  *
  */
 
-define('create_alias', "Create the alias and folder");
+define('create_alias', "Álnév és mappa létrehozása");
 define('save_as', "Mentés mint");
 define('failure', "Error, failed to generate the alias file");
 define('success', "Sikeres");
@@ -30,7 +30,7 @@ define('fast_download_service_for', "Downloads redirection service for %s");
 define('to_the_path', "To the path");
 define('at_url', "at URL");
 define('create_alias_for', "Create alias for");
-define('fast_dl', "Redirecting downloads (FastDL)");
+define('fast_dl', "Átirányított letöltések (FastDL)");
 define('current_aliases_at_remote_server', "Current aliases at remote server");
 define('delete_selected_aliases', "A kiválasztott álnevek törlése");
 define('no_aliases_defined', "There are no web aliases defined by OGP for this remote server yet.");
@@ -55,8 +55,8 @@ define('select_remote_server', "Válassz távoli szervert");
 define('access_rules', "Hozzáférési szabályok");
 define('create_aliases', "Álnév létrehozása");
 define('select_game', "Válassz játékot");
-define('games_without_specified_rules', "Games without specified rules");
-define('match_file_extension', "Match file extension");
+define('games_without_specified_rules', "Játékok speciális szabályok nélkül.");
+define('match_file_extension', "Fájl kiterjesztés egyezés");
 define('match_file_extension_info', "Specify extensions separated by coma,<br> the matching files will be accessible.<br><b>Blank for unrestricted access</b>.");
 define('match_client_ip', "Match client IP");
 define('match_client_ip_info', "Connections with matching IP will be granted,<br>blank for unrestricted access. You can use<br>multiple IPs or ranges separated by coma:<br>/xx subnets<br>Example: 10.0.0.0/16<br>/xxx.xxx.xxx.xxx subnets<br>Example: 10.0.0.0/255.0.0.0<br>Hyphen ranges<br>Example: 10.0.0.5-230<br>Asterisk matching<br>Example: 10.0.*.*");
@@ -64,7 +64,7 @@ define('save_access_rules', "Hozzáférési szabályok mentése");
 define('create_access_rules', "Hozzáférési szabályok létrehozása");
 define('invalid_entries_found', "Invalid entries found");
 define('game_name', "Játék neve");
-define('alias_already_exists', "Alias %s already exists.");
+define('alias_already_exists', "%s álnév már létezik.");
 define('warning_access_rules_applied_once_alias_created', "WARNING: Access rules are applied when the alias is created. No changes will be applied to the current aliases.");
 define('autostart_on_agent_startup', "Autostart on agent startup");
 define('autostart_on_agent_startup_info', "Start the fast download daemon automatically when the agent starts.");
