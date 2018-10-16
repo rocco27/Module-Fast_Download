77f7c61bf5f77de2fd3557405b2975135c6e4473
own3mall <own3mall@gmail.com>
2018-10-16 15:00:29 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Polish/modules/fast_download.php
+++ lang/Polish/modules/fast_download.php
@@ -22,7 +22,7 @@
  *
  */
 
-define('OGP_LANG_create_alias', "Create alias and folder");
+define('OGP_LANG_create_alias', "Utwórz alias i folder");
 define('OGP_LANG_save_as', "Zapisz jako");
 define('OGP_LANG_failure', "Błąd, nie udało się wygenerować pliku dla aliasu");
 define('OGP_LANG_success', "Powodzenie");
@@ -35,14 +35,14 @@ define('OGP_LANG_current_aliases_at_remote_server', "Bieżące aliasy na serwerz
 define('OGP_LANG_delete_selected_aliases', "Usuń wybrane aliasy");
 define('OGP_LANG_no_aliases_defined', "Nie istnieją jeszcze aliasy zdefiniowane przez OGP dla tego serwera hosta.");
 define('OGP_LANG_fastdl_port', "Port");
-define('OGP_LANG_fastdl_port_info', "Port on which your Fast Download Daemon will start.");
+define('OGP_LANG_fastdl_port_info', "Port na którym będzie działał/nasłuchiwał Fast Download.");
 define('OGP_LANG_fastdl_ip', "Adres");
 define('OGP_LANG_fastdl_ip_info', "Adres IP lub domeny, w której twój serwer Fast Download będzie działał, domena musi znajdować się w /etc/hosts.");
 define('OGP_LANG_listing', "Lista");
 define('OGP_LANG_listing_info', "Jeśli 'włączony', serwer wyświetli zawartość folderów.");
 define('OGP_LANG_fast_dl_advanced', "Zaawansowane Ustawienia");
 define('OGP_LANG_apply_settings_and_restart_fastdl', "Zapisz konfigurację oraz wykonaj restart daemona.");
-define('OGP_LANG_stop_fastdl', "Stop Fast Download Daemon");
+define('OGP_LANG_stop_fastdl', "Zatrzymaj daemon szybkiego pobierania");
 define('OGP_LANG_fast_download_daemon_running', "Fast Download Daemon jest uruchomiony.");
 define('OGP_LANG_fast_download_daemon_not_running', "Fast Download Daemon jest zatrzymany.");
 define('OGP_LANG_fastdl_could_not_be_restarted', "Nie można zrestartować usługi Fast Download.");
@@ -50,9 +50,9 @@ define('OGP_LANG_configuration_file_could_not_be_written', "Nie można zapisać 
 define('OGP_LANG_remove_folders', "Usuń folder dla wybranych aliasów.");
 define('OGP_LANG_remove_folder', "Usuń folder");
 define('OGP_LANG_delete_alias', "Usuń alias");
-define('OGP_LANG_no_game_homes_assigned', "You don't have any servers assigned to your account.");
+define('OGP_LANG_no_game_homes_assigned', "Nie masz żadnych serwerów przypisanych do twojego konta.");
 define('OGP_LANG_select_remote_server', "Wybierz serwer hosta");
-define('OGP_LANG_access_rules', "Access Rules");
+define('OGP_LANG_access_rules', "Zasady dostępu");
 define('OGP_LANG_create_aliases', "Stwórz Alias");
 define('OGP_LANG_select_game', "Wybierz grę");
 define('OGP_LANG_games_without_specified_rules', "Gry bez określonych reguł");
@@ -62,15 +62,15 @@ define('OGP_LANG_match_client_ip', "Dopasuj klienta IP");
 define('OGP_LANG_match_client_ip_info', "Połączenia z pasującym adresem IP zostaną przyznane,<br>
 puste dla nieograniczonego dostępu.<br>Możesz użyć wiele adresów IP lub zakresów oddzielonych przecinkami:<br>
 /xx podsieci<br> Przykład: 10.0.0.0/16<br> /xxx.xxx.xxx.xxx podsieci<br> Przykład: 10.0.0.0/255.0.0.0<br> Dzielniki<br> Przykład: 10.0.0.5-230<br> Gwiazdki<br> Przykład: 10.0.*.*");
-define('OGP_LANG_save_access_rules', "Save Access Rules");
+define('OGP_LANG_save_access_rules', "Zapisz zasady dostępu");
 define('OGP_LANG_create_access_rules', "Stwórz reguły dostępu");
 define('OGP_LANG_invalid_entries_found', "Znaleziono nieprawidłowe wpisy");
 define('OGP_LANG_game_name', "Nazwa Gry");
 define('OGP_LANG_alias_already_exists', "Alias %s już istnieje.");
 define('OGP_LANG_warning_access_rules_applied_once_alias_created', "UWAGA: Reguły dostępu są stosowane przy tworzeniu aliasu. W obecnych aliasach nie będą stosowane żadne zmiany.");
-define('OGP_LANG_autostart_on_agent_startup', "Autostart on Agent startup");
+define('OGP_LANG_autostart_on_agent_startup', "Autostart podczas uruchamiania agenta");
 define('OGP_LANG_autostart_on_agent_startup_info', "Start the fast download daemon automatically when the Agent starts.");
 define('OGP_LANG_port_forwarded_to_80', "Przekierowanie portu na 80");
 define('OGP_LANG_port_forwarded_to_80_info', "Włącz tą opcję gdy Fast Download będzie działał na domyślnym porcie 80,  port nie będzie wyświetlony w adresie URL");
-define('OGP_LANG_current_access_rules', "Current access rules");
+define('OGP_LANG_current_access_rules', "Aktualne zasady dostępu");
 ?>
