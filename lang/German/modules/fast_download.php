615f30f0c49775e6adc73cd1c3aceeb8960b8d15
own3mall <own3mall@gmail.com>
2018-11-03 04:58:50 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/German/modules/fast_download.php
+++ lang/German/modules/fast_download.php
@@ -22,7 +22,7 @@
  *
  */
 
-define('OGP_LANG_create_alias', "Create alias and folder");
+define('OGP_LANG_create_alias', "Alias und Ordner erstellen");
 define('OGP_LANG_save_as', "Speichern als");
 define('OGP_LANG_failure', "Fehler, konnte die Alias-Datei nicht generieren");
 define('OGP_LANG_success', "Erfolg");
@@ -50,9 +50,9 @@ define('OGP_LANG_configuration_file_could_not_be_written', "Die Konfigurationsda
 define('OGP_LANG_remove_folders', "Ordner für ausgewählte Aliase entfernen.");
 define('OGP_LANG_remove_folder', "Ordner löschen");
 define('OGP_LANG_delete_alias', "Alias löschen");
-define('OGP_LANG_no_game_homes_assigned', "You don't have any servers assigned to your account.");
+define('OGP_LANG_no_game_homes_assigned', "Ihrem Account sind keine Server zugewiesen.");
 define('OGP_LANG_select_remote_server', "Remote Server auswählen");
-define('OGP_LANG_access_rules', "Access Rules");
+define('OGP_LANG_access_rules', "Zugriffsregeln");
 define('OGP_LANG_create_aliases', "Aliase erstellen");
 define('OGP_LANG_select_game', "Spiel auswählen");
 define('OGP_LANG_games_without_specified_rules', "Spiele ohne spezifizierte Regeln");
@@ -60,7 +60,7 @@ define('OGP_LANG_match_file_extension', "Identische Dateierweiterung");
 define('OGP_LANG_match_file_extension_info', "Zugriffe via Kommagetrennte Dateierweiterungen erlauben.<br> <b>Leer für uneingeschränkten Zugriff</b>.");
 define('OGP_LANG_match_client_ip', "Client IP identisch");
 define('OGP_LANG_match_client_ip_info', "Zugriff für eingegebene IP's erlauben,<br>leer für uneingeschränkten Zugriff. Sie können auch<br>mehrere IP Adressen oder ganze IP Ranges kommagetrennt einfügen.<br>/xx Subnetze<br>Beispiel: 10.0.0.0/16<br>/xxx.xxx.xxx.xxx Subnetze<br>Beispiel: 10.0.0.0/255.0.0.0<br>Hyphen Ranges<br>Beispiel: 10.0.0.5-230<br>Asterisk Treffer<br>Beispiel: 10.0.*.*");
-define('OGP_LANG_save_access_rules', "Save Access Rules");
+define('OGP_LANG_save_access_rules', "Zugriffsregeln speichern");
 define('OGP_LANG_create_access_rules', "Zugriffsregeln erstellen");
 define('OGP_LANG_invalid_entries_found', "Ungültige Einträge gefunden");
 define('OGP_LANG_game_name', "Spielname");
@@ -70,5 +70,5 @@ define('OGP_LANG_autostart_on_agent_startup', "Autostart beim Agentenstart");
 define('OGP_LANG_autostart_on_agent_startup_info', "Starten Sie den schnellen Download Daemon automatisch, wenn der Agent startet.");
 define('OGP_LANG_port_forwarded_to_80', "Port weitergeleitet von 80");
 define('OGP_LANG_port_forwarded_to_80_info', "Aktivieren Sie diese Option, wenn der für diesen FastDL Daemon konfigurierte Port von Port 80 aus weitergeleitet wurde, sodass der Port bei URLs verborgen bleibt.");
-define('OGP_LANG_current_access_rules', "Current access rules");
+define('OGP_LANG_current_access_rules', "Aktuelle Zugriffsregeln");
 ?>
